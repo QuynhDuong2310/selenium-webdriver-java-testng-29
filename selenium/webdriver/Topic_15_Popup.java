49ff1daf6b2a0b01e565cf218dc08336bae0a1a6
QuynhDuong2310 <Quynhxoansh95@gmail.com>
2023-12-01 20:59:31 +0700

    Adding topic No.10 and no.16
    Signed-off-by: QuynhDuong2310 <Quynhxoansh95@gmail.com>

--- selenium/webdriver/Topic_15_Popup.java
+++ selenium/webdriver/Topic_15_Popup.java
@@ -1,15 +1,17 @@
+
+
 package webdriver;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Cookie;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -25,9 +27,8 @@ public class Topic_15_Popup {
         System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
         driver = new FirefoxDriver();
         explicitWait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.manage().window().maximize();
-
     }
 
     @Test
@@ -45,7 +46,6 @@ public class Topic_15_Popup {
         driver.findElement(By.cssSelector("div[id='modal-login-v1'][style] button.close")).click();
         Assert.assertFalse(loginPopup.isDisplayed());
 
-
     }
 
     @Test
@@ -54,10 +54,8 @@ public class Topic_15_Popup {
         driver.findElement(By.cssSelector("div.nav-item a.login-btn")).click();
         WebElement kynaLoginPopup = driver.findElement(By.cssSelector("div.k-popup-account-mb-content"));
         Assert.assertTrue(kynaLoginPopup.isDisplayed());
-
         driver.findElement(By.cssSelector("div.k-popup-account-mb-content input#user-login")).sendKeys("fcautomation");
         driver.findElement(By.cssSelector("div.k-popup-account-mb-content input#user-password")).sendKeys("fcautomation");
-
 
         driver.findElement(By.cssSelector("div.k-popup-account-mb-content button#btn-submit-login")).click();
         String kynaErrorMessage = explicitWait.until(ExpectedConditions.visibilityOfElementLocated((By.cssSelector("div.k-popup-account-mb-content div#password-form-login-message")))).getText();
@@ -65,22 +63,18 @@ public class Topic_15_Popup {
         Assert.assertEquals(kynaErrorMessage, "Sai tên đăng nhập hoặc mật khẩu");
         driver.findElement(By.cssSelector("div.k-popup-account-mb-content button.k-popup-account-close")).click();
         Assert.assertFalse(kynaLoginPopup.isDisplayed());
-
         // Làm sao để verify đc 1 element khi nó k còn trong DOM?
         //boolean elementStatus = explicitWait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector("div.k-popup-account-mb-content div#password-form-login-message"))));
         boolean elementStatus = explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.k-popup-account-mb-content div#password-form-login-message")));
         Assert.assertTrue(elementStatus);
-
         List<WebElement> popup = driver.findElements(By.xpath(""));
         Assert.assertEquals(popup.size(), 0);
-
 
     }
 
     @Test
     public void TC_03_Fixed_Popup_Not_In_DOM() {
         driver.get("");
-
     }
 
     @Test
@@ -89,7 +83,6 @@ public class Topic_15_Popup {
         // Nếu popup is not displayed >> is the testcase will be marked as fail?
         driver.manage().deleteAllCookies();
         driver.navigate().refresh();
-
         List<WebElement> popupA = driver.findElements(By.xpath("//div[@class='lepopup-popup-container']//div[@class='lepopup-form-inner']/parent::div[not(contains(@style,'display:none;'))]"));
         System.out.println(popupA.size());
         if (!popupA.isEmpty()) {
@@ -98,7 +91,6 @@ public class Topic_15_Popup {
             sleepInSecond(3);
             Assert.assertEquals(popupA.size(), 1);
             System.out.println("Tới đây rồi nè Q!");
-
         } else {
             driver.findElement(By.cssSelector("input#search-input")).sendKeys("Agile Testing Explained");
             //ul[@id='posts-container']//li
@@ -106,9 +98,7 @@ public class Topic_15_Popup {
             String resultSearchList = driver.findElement(By.xpath("//ul[@id='posts-container']/li[1]//h2[@class='post-title']//a")).getText();
             Assert.assertEquals(resultSearchList, "Agile Testing Explained");
             System.out.println("Tới đây rồi nè Q ơi!");
-
         }
-
     }
 
     @Test
@@ -122,12 +112,10 @@ public class Topic_15_Popup {
             sleepInSecond(3);
             Assert.assertEquals(kynaLoginPopup.size(), 1);
             System.out.println("Tới đây rồi nè Q!");
-
         } else {
             System.out.println("Tới đây rồi nè Q ơi!");
         }
     }
-
 
     @Test
     public void TC_06_Random_Popup_In_DOM() {
@@ -143,7 +131,6 @@ public class Topic_15_Popup {
             sleepInSecond(3);
             Assert.assertEquals(eduRandomPopup.size(), 1);
             System.out.println("Tới đây rồi nè Q!");
-
         } else {
             System.out.println("Tới đây rồi nè Q ơi!");
         }
@@ -151,7 +138,53 @@ public class Topic_15_Popup {
 
     @Test
     public void TC_07_Random_Popup_Not_In_DOM() {
+        driver.get("https://www.javacodegeeks.com/");
+        By randomJavaBy = By.cssSelector("div.lepopup-popup-container>div:not([style^='display:none'])");
+        if (driver.findElements(randomJavaBy).size() > 0 && driver.findElements(randomJavaBy).get(0).isDisplayed()) {
+            // Vẫn có trong DOM nhưng không hiển thị lên UI >> Dùng hàm isDisplayed thì vẫn có thể kiểm tra đc
+            // Nhunge nếu mà page chưa load xong >> popup chưa đc render ra >> Thì cái popup sẽ k có trong DOM >> Dẫn đến hàm findElement() sẽ đánh fail test case.
+            System.out.println("Popup hiển thị");
+            driver.findElement(By.xpath("//a[contains(text(),'×')]")).click();
+            sleepInSecond(3);
+        }
+        driver.findElement(By.cssSelector("input#search-input")).sendKeys("Agile Testing Explained");
+        driver.findElement(By.xpath("//button[@id='search-submit']")).click();
+        String resultSearchList = driver.findElement(By.xpath("//a[text()='Agile Testing Explained']")).getText();
+        Assert.assertEquals(resultSearchList, "Agile Testing Explained");
 
+    }
+
+    @Test
+    public void TC_08_Random_Popup_In_DOM() {
+        driver.get("https://vnk.edu.vn/");
+        sleepInSecond(30);
+        By marketingPopup = By.cssSelector("div.tve-leads-conversion-object");
+        if (driver.findElement(marketingPopup).isDisplayed()) {
+            driver.findElement(By.cssSelector("div.tve_ea_thrive_leads_form_close")).click();
+            sleepInSecond(3);
+            System.out.println("Popup hiển thị");
+        } else {
+            System.out.println("Popup không hiển thị");
+        }
+        driver.findElement(By.xpath("//button[text()='Danh sách khóa học']")).click();
+        sleepInSecond(2);
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.title-content>h1")).getText(), "Lịch Khai Giảng");
+    }
+
+    @Test
+    public void TC_09_Random_Popup_Not_In_DOM() {
+        driver.get("https://dehieu.vn/");
+        By marketingPopup = By.cssSelector("div.popup-content");
+        if (driver.findElements(marketingPopup).size() > 0 && driver.findElements(marketingPopup).get(0).isDisplayed()) {
+            System.out.println("Popup hiển thị");
+            int heightBrowser = driver.manage().window().getSize().getHeight();
+            if (heightBrowser < 1920) {
+                ((JavascriptExecutor) driver).executeScript("arguments[0].click()", driver.findElement(By.cssSelector("button#close-popup")));
+            } else {
+                driver.findElement(By.cssSelector("button#close-popup")).click();
+            }
+            sleepInSecond(3);
+        }
     }
 
     @AfterClass
